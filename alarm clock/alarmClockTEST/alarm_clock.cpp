a8daf646608b9a1b296dee64d0941e93d253c02c
Krzysztof Druciak <krzysztof.druciak@ericsson.com>
2016-12-23 15:29:19 +0100

    dodano klase do poberania wschodow i zachodow slonca

--- alarm clock/alarmClockTEST/alarm_clock.cpp
+++ alarm clock/alarmClockTEST/alarm_clock.cpp
@@ -7,9 +7,14 @@ Alarm_clock::Alarm_clock()
 
 void Alarm_clock::setAlarm(std::string alarmName, std::string day, int h, int m)
 {
-    alarmMap[alarmName].alarmMap[day];
-    auto a  =alarmMap[alarmName].alarmMap.find(day);
-    a->second.set(h,m);
+    std::vector <std::string> dayVector = dummyForSplitString.split(day,"-");
+    //std::cout << "vector size: "<< dayVector.size()<< " day "<< day <<std::endl;
+    for (auto day: dayVector){
+        std::cout << " day: " << day <<std::endl;
+        alarmMap[alarmName].alarmMap[day];
+        auto a  =alarmMap[alarmName].alarmMap.find(day);
+        a->second.set(h,m);
+    }
 }
 
 void Alarm_clock::addAlarm(std::string alarmName)
@@ -31,7 +36,7 @@ void Alarm_clock::listAlarm()
     for (auto a : alarmMap){
 
         for (auto b : alarmMap[a.first].alarmMap){
-            std::cout <<"alarm: "<<a.first <<" set: " <<b.first << " hour: "<< b.second.hour << " minutes: " << b.second.min <<std::endl;
+            std::cout <<"alarm: "<<a.first <<"\tset: " <<b.first << " hour: "<< b.second.hour << " minutes: " << b.second.min <<std::endl;
         }
     }
 
--- alarm clock/alarmClockTEST/alarm_clock.h
+++ alarm clock/alarmClockTEST/alarm_clock.h
@@ -2,6 +2,7 @@
 #define ALARM_CLOCK_H
 #include <map>
 #include <iostream>
+#include <vector>
 struct Day{
     bool MON = false;
     bool TUE = false;
@@ -22,6 +23,20 @@ struct Clock {
         this->isSet = true;
         std::cout << " ustawiono " << h <<":"<<m <<std::endl;
     }
+    std::vector<std::string> split( const std::string& stringToSplit, const std::string& delimiters) {
+       using namespace std;
+       // Skip all the text until the first delimiter is found
+       string::size_type start = stringToSplit.find_first_not_of(delimiters, 0);
+       string::size_type stop = stringToSplit.find_first_of(delimiters, start);
+
+       std::vector<std::string> tokens;
+       while (string::npos != stop || string::npos != start) {
+          tokens.push_back(stringToSplit.substr(start, stop - start));
+          start = stringToSplit.find_first_not_of(delimiters, stop);
+          stop = stringToSplit.find_first_of(delimiters, start);
+       }
+       return tokens;
+ }
 };
 struct Alarm {
     std::map < std::string ,Clock > alarmMap;
@@ -30,6 +45,7 @@ struct Alarm {
 class Alarm_clock
 {
     std::map <std::string, Alarm> alarmMap;
+    Clock dummyForSplitString;
 public:
     Alarm_clock();
     void checkAlarm();
