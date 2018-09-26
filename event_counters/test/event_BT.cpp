1f9d46b942fb0ffdf76850333d49ae4d4e36fc94
cyniu <cyniu88@wp.pl>
2018-09-26 15:49:20 +0200

    nowe testy eventu

---
+++ event_counters/test/event_BT.cpp
@@ -0,0 +1,79 @@
+#include <gtest/gtest.h>
+#include "test_data.h"
+#include "../../../iDom_server_OOP/src/iDomTools/test/iDomTools_fixture.h"
+#include "../event_counters_handler.h"
+
+class event_counter_fixture : public iDomTOOLS_ClassTest
+{
+public:
+     event_counters_handler mainEvent;
+     std::string msg = "info";
+     std::string testEvent = "testEvent";
+     void preper1001Event(){
+         int counter = 1000;
+         EXPECT_EQ(mainEvent.run(testEvent)->howManyEvent(),0);
+         mainEvent.run(testEvent)->addEvent(msg);
+         EXPECT_EQ(mainEvent.run(testEvent)->howManyEvent(),1);
+         std::string returnedString = mainEvent.run(testEvent)->getEvent();
+         EXPECT_THAT(returnedString, testing::HasSubstr(msg));
+         EXPECT_EQ(mainEvent.run(testEvent)->howManyEvent(),1);
+
+         for (int i = 1; i!= counter; counter--)
+         {
+             mainEvent.run(testEvent)->addEvent(msg);
+             if(counter == 500)
+                 mainEvent.run(testEvent)->addEvent("cyniu");
+         }
+         EXPECT_EQ(mainEvent.run(testEvent)->howManyEvent(),1001);
+         returnedString = mainEvent.run(testEvent)->getEvent();
+         EXPECT_THAT(returnedString, testing::HasSubstr("cyniu"));
+     }
+};
+
+TEST_F(event_counter_fixture, add_get_clear_event)
+{
+    EXPECT_EQ(mainEvent.run(testEvent)->howManyEvent(),0);
+    mainEvent.run(testEvent)->addEvent(msg);
+    EXPECT_EQ(mainEvent.run(testEvent)->howManyEvent(),1);
+    std::string returnedString = mainEvent.run(testEvent)->getEvent();
+    EXPECT_THAT(returnedString, testing::HasSubstr(msg));
+    EXPECT_EQ(mainEvent.run(testEvent)->howManyEvent(),1);
+    mainEvent.run(testEvent)->clearEvent();
+    EXPECT_EQ(mainEvent.run(testEvent)->howManyEvent(),0);
+}
+
+TEST_F(event_counter_fixture, clear_middle_part_of_event_small_big)
+{
+    preper1001Event();
+    mainEvent.run(testEvent)->clearEvent(400,600);
+    EXPECT_EQ(mainEvent.run(testEvent)->howManyEvent(),801);
+    std::string returnedString = mainEvent.run(testEvent)->getEvent();
+    EXPECT_THAT(returnedString, testing::Not(testing::HasSubstr("cyniu")));
+}
+
+TEST_F(event_counter_fixture, clear_middle_part_of_event_big_small)
+{
+    preper1001Event();
+    mainEvent.run(testEvent)->clearEvent(600,400);
+    EXPECT_EQ(mainEvent.run(testEvent)->howManyEvent(),1001);
+    std::string returnedString = mainEvent.run(testEvent)->getEvent();
+    EXPECT_THAT(returnedString,testing::HasSubstr("cyniu"));
+}
+
+TEST_F(event_counter_fixture, clear_middle_part_of_event_to_max)
+{
+    preper1001Event();
+    mainEvent.run(testEvent)->clearEvent(400,1600);
+    EXPECT_EQ(mainEvent.run(testEvent)->howManyEvent(),400);
+    std::string returnedString = mainEvent.run(testEvent)->getEvent();
+    EXPECT_THAT(returnedString, testing::Not(testing::HasSubstr("cyniu")));
+}
+
+TEST_F(event_counter_fixture, clear_middle_part_of_event_from_max)
+{
+    preper1001Event();
+    mainEvent.run(testEvent)->clearEvent(1400,1600);
+    EXPECT_EQ(mainEvent.run(testEvent)->howManyEvent(),1001);
+    std::string returnedString = mainEvent.run(testEvent)->getEvent();
+    EXPECT_THAT(returnedString, testing::HasSubstr("cyniu"));
+}
