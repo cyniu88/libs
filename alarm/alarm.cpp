fee75ed7ca5706068debabc6e74465ae75a9583d
cyniu88 <cyniu88@wp.pl>
2022-03-21 18:05:18 +0100

    poprawka spacji

--- alarm/alarm.cpp
+++ alarm/alarm.cpp
@@ -7,19 +7,19 @@ alarm_C::alarm_C()
 
 void alarm_C::raiseAlarm(int id, const std::string &txt)
 {
-    std::lock_guard <std::mutex> lock (mutex_alarm);
+    std::lock_guard <std::mutex> lock(mutex_alarm);
     m_alarmMap[id] = txt;
 }
 
 void alarm_C::clearAlarm(int id)
 {
-    std::lock_guard <std::mutex> lock (mutex_alarm);
+    std::lock_guard <std::mutex> lock(mutex_alarm);
     m_alarmMap.erase(id);
 }
 
 std::string alarm_C::showAlarm()
 {
-    std::lock_guard <std::mutex> lock (mutex_alarm);
+    std::lock_guard <std::mutex> lock(mutex_alarm);
     std::stringstream ret;
 
     if(m_alarmMap.empty())
@@ -34,6 +34,6 @@ std::string alarm_C::showAlarm()
 
 std::size_t alarm_C::alarmSize()
 {
-    std::lock_guard <std::mutex> lock (mutex_alarm);
+    std::lock_guard <std::mutex> lock(mutex_alarm);
     return m_alarmMap.size();
 }
