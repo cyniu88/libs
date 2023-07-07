b46adff3364064937dd75c5a84d6c8d511f78b85
cyniu88 <cyniu88@wp.pl>
2023-07-07 10:50:24 +0200

    format dokumentu

--- backTrace/backTrace.h
+++ backTrace/backTrace.h
@@ -1,21 +1,21 @@
 #ifndef BACKTRACE_H
 #define BACKTRACE_H
 
-
 #include <execinfo.h>
 #include <signal.h>
 #include <cxxabi.h> // demangle
 #include <iostream>
 
 /// add link option ADD_LINK_OPTIONS(-rdynamic)  to cmake file
-namespace cyniu{
+namespace cyniu
+{
 
   namespace BACKTRACE
   {
     void handleCrash(int sig)
     {
 
-      void* array[100];
+      void *array[100];
 
       // Signal catched
       fprintf(stderr, "handleCrash() signal received is: %d\n", sig);
@@ -25,63 +25,63 @@ namespace cyniu{
       fprintf(stderr, "backtrace() returned addresses: %d\n", n_addresses);
 
       // Instead of 'backtrace_symbols_fd(array, n_addresses, STDERR_FILENO)', we will iterate to demangle symbols:
-      char** addresses = backtrace_symbols(array, n_addresses);
+      char **addresses = backtrace_symbols(array, n_addresses);
 
       if (addresses == NULL)
-        {
-          perror("backtrace_symbols");
-          exit(EXIT_FAILURE);
-        }
+      {
+        perror("backtrace_symbols");
+        exit(EXIT_FAILURE);
+      }
 
       // Demangle backtrace addresses:
       std::string address, symbol;
       // This check is removed.
       // cppcheck-suppress variableScope
       size_t openPos, closePos, plusPos;
-      char* demangledName;
+      char *demangledName;
       int status;
 
       for (int k = 0; k < n_addresses; k++)
+      {
+        // Address example: ./a.out(_Z16displayBacktracev) [0x556df272d3f6]
+        // Need to extract the symbol within brackets:
+        address = addresses[k];
+        openPos = address.find("(") + 1;
+        closePos = address.find(")");
+
+        if (closePos > openPos)
         {
-          // Address example: ./a.out(_Z16displayBacktracev) [0x556df272d3f6]
-          // Need to extract the symbol within brackets:
-          address = addresses[k];
-          openPos = address.find("(") + 1;
-          closePos = address.find(")");
+          symbol = address.substr(openPos, closePos - openPos);
+          plusPos = symbol.find("+");
 
-          if (closePos > openPos)
-            {
-              symbol = address.substr(openPos, closePos - openPos);
-              plusPos = symbol.find("+");
+          if (plusPos > 1)
+          {
+            symbol = symbol.substr(0, plusPos);
+          }
 
-              if (plusPos > 1)
-                {
-                  symbol = symbol.substr(0, plusPos);
-                }
+          status = -1;
+          demangledName = abi::__cxa_demangle(symbol.c_str(), NULL, NULL, &status);
 
-              status = -1;
-              demangledName = abi::__cxa_demangle(symbol.c_str(), NULL, NULL, &status );
-
-              if (status == 0)
-                {
-                  fprintf(stderr, "%s\n", demangledName);
-                }
-              else
-                {
-                  fprintf(stderr, "%s\n", address.c_str());
-                }
-
-              free( demangledName );
-            }
+          if (status == 0)
+          {
+            fprintf(stderr, "%s\n", demangledName);
+          }
           else
-            {
-              fprintf(stderr, "%s\n", address.c_str());
-            }
+          {
+            fprintf(stderr, "%s\n", address.c_str());
+          }
+
+          free(demangledName);
         }
+        else
+        {
+          fprintf(stderr, "%s\n", address.c_str());
+        }
+      }
 
       free(addresses);
-     // raise(SIGSEGV);
-     exit(1);
+
+      exit(1);
     }
   } // BACKTRACE
 } // cyniu
