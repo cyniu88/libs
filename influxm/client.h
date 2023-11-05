8de582cb0c8b818e6f3c27eba12b7fa066c52d8a
cyniu88 <cyniu88@wp.pl>
2023-11-05 23:59:21 +0100

    influx klient

---
+++ influxm/client.h
@@ -0,0 +1,328 @@
+//
+// Created by Myriad-Dreamin on 2021/8/13.
+//
+
+#ifndef INFLUXDBM_CLIENT_H
+#define INFLUXDBM_CLIENT_H
+
+#include "http.h"
+#include "macro.h"
+#include <cassert>
+#include <chrono>
+#include <tuple>
+#include <type_traits>
+#include <vector>
+
+#ifndef influxdb_if_inline
+#define influxdb_if_inline inline
+#endif
+
+namespace influx_client {
+struct kv_t;
+using tag = kv_t;
+using field = kv_t;
+using point_vec = std::vector<influx_client::detail::string_view_t>;
+
+namespace detail {
+template <typename T, typename X = void>
+using is_influx_string_t = typename std::enable_if<
+    std::is_same<T, detail::string_view_ref>::value ||
+        std::is_same<T, std::string>::value ||
+        std::is_same<const char *, T>::value || std::is_same<char *, T>::value,
+    X>::type;
+
+template <typename T, typename X = void>
+using is_influx_boolean_t =
+    typename std::enable_if<std::is_same<T, bool>::value, X>::type;
+
+template <typename T, typename X = void>
+using is_influx_integer_t = typename std::enable_if<
+    std::is_arithmetic<T>::value && !std::is_same<T, bool>::value, X>::type;
+
+} // namespace detail
+
+struct kv_t {
+  std::string k;
+  std::string v;
+  bool q;
+  template <typename T>
+  kv_t(
+      detail::string_view_ref k, T v,
+      macroPAssert(detail::is_influx_string_t, T))
+      : k(k), v(v), q(true) {}
+  template <typename T>
+  kv_t(
+      detail::string_view_ref k, T v,
+      macroPAssert(detail::is_influx_boolean_t, T))
+      : k(k), v(v ? "true" : "false"), q(false) {}
+  template <typename T>
+  kv_t(
+      detail::string_view_ref k, T v,
+      macroPAssert(detail::is_influx_integer_t, T))
+      : k(k), v(std::to_string(v)), q(false) {}
+};
+
+namespace flux {
+struct Client {
+  using timestamp_t = uint64_t;
+
+  struct sockaddr_in addr {};
+  std::string host;
+  std::string bucket;
+  std::string organization;
+  std::string token;
+  std::string precision;
+  std::string write_v2_header;
+  int port;
+
+  Client(
+      detail::string_view_ref host, int port, detail::string_view_ref token,
+      detail::string_view_ref org, detail::string_view_ref bucket)
+      // detail::string_view precision = "ns"
+      : host(host), port(port), token(token), organization(org), bucket(bucket),
+        precision("ns") {
+    reset_network_data();
+  }
+
+  void reset_network_data() {
+    write_v2_header.resize(400);
+    int res = (snprintf(
+        &write_v2_header[0], write_v2_header.size(),
+        "%s /%s?bucket=%s&org=%s&precision=%s HTTP/1.1\r\n"
+        "Host: %s\r\nAuthorization: Token %s\r\n",
+        "POST", "api/v2/write", bucket.c_str(), organization.c_str(),
+        precision.c_str(), host.c_str(), token.c_str()));
+    assert(res <= 400);
+    write_v2_header.resize(res);
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    if ((addr.sin_addr.s_addr = inet_addr(host.c_str())) == INADDR_NONE) {
+      abort();
+    }
+  }
+
+  template <typename T, typename F>
+  int createPoint(
+      detail::string_view_ref measurement, T tags_begin, T tags_end,
+      F fields_begin, F fields_end, char *buf, int bufSize, timestamp_t t = 0);
+
+  int write(detail::string_view_ref point, std::string *resp = nullptr);
+  template <typename T> int writes(T points, std::string *resp = nullptr);
+
+  template <typename T, typename F>
+  int writes(
+      std::initializer_list<
+          std::tuple<detail::string_view_ref, T, T, F, F, timestamp_t>>
+          points,
+      char *buf, int bufSize, std::string *resp = nullptr);
+  template <typename T> int writeIter(T pb, T pe, std::string *resp = nullptr);
+
+  template <typename T, typename F>
+  int writeIter(
+      detail::string_view_ref measurement, T tb, T te, F fb, F fe,
+      timestamp_t t = 0, std::string *resp = nullptr);
+
+#define macroWriteImpl(T, F)                                                   \
+  int write(                                                                   \
+      detail::string_view_ref measurement, T tags, F fields,                   \
+      timestamp_t t = 0, std::string *resp = nullptr) {                        \
+    return writeIter(                                                          \
+        measurement, tags.begin(), tags.end(), fields.begin(), fields.end(),   \
+        t, resp);                                                              \
+  }
+
+  template <typename T, typename F> macroWriteImpl(T, F);
+  template <typename T> macroWriteImpl(T, std::initializer_list<kv_t>);
+  template <typename T> macroWriteImpl(std::initializer_list<kv_t>, T);
+  macroWriteImpl(std::initializer_list<kv_t>, std::initializer_list<kv_t>);
+#undef macroWriteImpl
+
+#define macroCratePointImpl(T, F)                                              \
+  int createPoint(                                                             \
+      detail::string_view_ref measurement, T tags, F fields, char *buf,        \
+      int bufSize, timestamp_t t = 0) {                                        \
+    return createPoint(                                                        \
+        measurement, tags.begin(), tags.end(), fields.begin(), fields.end(),   \
+        buf, bufSize, t);                                                      \
+  }
+
+  template <typename T, typename F> macroCratePointImpl(T, F);
+  template <typename T> macroCratePointImpl(T, std::initializer_list<kv_t>);
+  template <typename T> macroCratePointImpl(std::initializer_list<kv_t>, T);
+  macroCratePointImpl(std::initializer_list<kv_t>, std::initializer_list<kv_t>);
+#undef macroCratePointImpl
+
+#define macroWritesImpl(T, F)                                                  \
+  int writes(                                                                  \
+      std::initializer_list<                                                   \
+          std::tuple<detail::string_view_t, T, F, timestamp_t>>                \
+          raw_points,                                                          \
+      char *buf, int bufSize, std::string *resp = nullptr) {                   \
+    int code, offset = 0;                                                      \
+    point_vec Vec;                                                             \
+    for (auto &p : raw_points) {                                               \
+      code = createPoint(                                                      \
+          std::get<0>(p), std::get<1>(p).begin(), std::get<1>(p).end(),        \
+          std::get<2>(p).begin(), std::get<2>(p).end(), buf + offset,          \
+          bufSize - offset, std::get<3>(p));                                   \
+      if (code < 0) {                                                          \
+        return code;                                                           \
+      }                                                                        \
+      Vec.emplace_back(buf + offset, code);                                    \
+      offset += code;                                                          \
+    }                                                                          \
+    return writes(Vec, resp);                                                  \
+  }
+
+  template <typename T, typename F> macroWritesImpl(T, F);
+  template <typename T> macroWritesImpl(T, std::initializer_list<kv_t>);
+  template <typename T> macroWritesImpl(std::initializer_list<kv_t>, T);
+  macroWritesImpl(std::initializer_list<kv_t>, std::initializer_list<kv_t>);
+#undef macroWritesImpl
+};
+} // namespace flux
+
+namespace detail {
+
+template <typename B, typename T>
+int putKVSeq(B &buf, int64_t &q, int64_t bufSize, T b, T e) {
+  for (auto v = b; v != e; v++) {
+    macroMemoryPutC(buf, ',', q, bufSize);
+    macroMemoryPutStdStr(buf, v->k, q, bufSize);
+    macroMemoryPutC(buf, '=', q, bufSize);
+    if (v->q) {
+      macroMemoryPutC(buf, '"', q, bufSize);
+      size_t pos, start = 0;
+      while ((pos = v->v.find_first_of('\"', start)) != std::string::npos) {
+        macroMemoryCopyN(buf, v->v.c_str() + start, pos - start, q, bufSize);
+        macroMemoryPutC(buf, '\\', q, bufSize);
+        macroMemoryPutC(buf, v->v[pos], q, bufSize);
+        start = ++pos;
+      }
+      macroMemoryCopyN(
+          buf, v->v.c_str() + start, v->v.length() - start, q, bufSize);
+      macroMemoryPutC(buf, '"', q, bufSize);
+    } else {
+      macroMemoryPutStdStr(buf, v->v, q, bufSize);
+    }
+  }
+  return 0;
+}
+
+} // namespace detail
+
+int flux::Client::write(detail::string_view_ref point, std::string *resp) {
+  return detail::http_request(&addr, write_v2_header, point, resp);
+}
+
+template <typename T, typename F>
+int flux::Client::createPoint(
+    detail::string_view_ref measurement, T tags_begin, T tags_end,
+    F fields_begin, F fields_end, char *buf, int bufSize,
+    flux::Client::timestamp_t t) {
+  int64_t q = 0;
+  int code;
+  macroMemoryPutStdStr(buf, measurement, q, bufSize);
+
+  if (tags_begin != tags_end) {
+    code = detail::putKVSeq(buf, q, bufSize, tags_begin, tags_end);
+    if (code) {
+      return code;
+    }
+  }
+
+  if (fields_begin == fields_end) {
+    assert(false);
+  }
+  int b = q;
+  code = detail::putKVSeq(buf, q, bufSize, fields_begin, fields_end);
+  buf[b] = ' ';
+  if (code) {
+    return code;
+  }
+  macroMemoryPutC(buf, ' ', q, bufSize);
+
+  if (!t) {
+    t = std::chrono::duration_cast<std::chrono::nanoseconds>(
+            std::chrono::high_resolution_clock::now().time_since_epoch())
+            .count();
+  }
+  std::string ts = std::to_string(t);
+  macroMemoryPutStdStr(buf, ts, q, bufSize);
+  return q;
+}
+template <typename T> int flux::Client::writes(T points, std::string *resp) {
+  return writeIter(points.begin(), points.end(), resp);
+}
+
+template <typename T, typename F>
+int flux::Client::writeIter(
+    detail::string_view_ref measurement, T tb, T te, F fb, F fe, timestamp_t t,
+    std::string *resp) {
+  macroAllocBuffer(buf, bufSize);
+  int code = createPoint(measurement, tb, te, fb, fe, buf, bufSize, t);
+  if (code < 0) {
+    return code;
+  }
+  code = detail::http_request(
+      &addr, write_v2_header, detail::string_view_t(buf, code), resp);
+  macroFreeBuffer(buf, bufSize);
+  return code;
+}
+
+template <typename T>
+int flux::Client::writeIter(T pb, T pe, std::string *resp) {
+  int sock = detail::create_socket(&addr);
+  if (sock < 0) {
+    return sock;
+  }
+  std::vector<iovec> Iov;
+  Iov.resize(4);
+  Iov.reserve(7);
+  int body_size = 0;
+  for (auto p = pb; p != pe; p++) {
+    if (p->size() > 65535) {
+      return -1;
+    }
+    if (body_size) {
+      Iov.emplace_back(iovec{(void *)"\n", 1});
+      body_size++;
+    }
+    Iov.emplace_back(iovec{
+        reinterpret_cast<void *>(const_cast<char *>(&(*p)[0])), p->size()});
+    body_size += p->size();
+    if (body_size > 65535) {
+      return -1;
+    }
+  }
+
+  int ret = detail::http_request_v_(
+      sock, write_v2_header, &Iov[0], Iov.size(), body_size, resp);
+  closesocket(sock);
+  return ret;
+}
+template <typename T, typename F>
+int flux::Client::writes(
+    std::initializer_list<
+        std::tuple<detail::string_view_ref, T, T, F, F, timestamp_t>>
+        points,
+    char *buf, int bufSize, std::string *resp) {
+  int code, offset = 0;
+  point_vec Vec;
+  for (auto p : points) {
+    code = createPoint(
+        std::get<0>(p), std::get<1>(p), std::get<2>(p), std::get<3>(p),
+        std::get<4>(p), buf + offset, bufSize - offset, std::get<5>(p));
+    if (code < 0) {
+      return code;
+    }
+    Vec.emplace_back(points + offset, code);
+    offset += code;
+  }
+  return writes(Vec, resp);
+}
+
+} // namespace influx_client
+
+#endif // INFLUXDBM_CLIENT_H
---
+++ influxm/http.h
@@ -0,0 +1,282 @@
+//
+// Created by Myriad-Dreamin on 2021/8/13.
+//
+
+#ifndef INFLUXDBM_HTTP_H
+#define INFLUXDBM_HTTP_H
+
+#include "macro.h"
+#include <algorithm>
+
+#ifdef _WIN32
+#define NOMINMAX
+#include <windows.h>
+#pragma comment(lib, "ws2_32")
+typedef struct iovec {
+  void *iov_base;
+  size_t iov_len;
+} iovec;
+#ifndef writev
+#define writev writev
+influxdb_if_inline uint64_t writev(int sock, struct iovec *iov, int cnt) {
+  uint64_t r = send(sock, (const char *)iov->iov_base, iov->iov_len, 0);
+  return (r < 0 || cnt == 1) ? r : r + writev(sock, iov + 1, cnt - 1);
+}
+#endif
+
+#else
+
+extern "C" {
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+}
+
+#define closesocket close
+#endif
+
+namespace influx_client {
+namespace detail {
+
+influxdb_if_inline constexpr uint8_t bit_swap8(uint8_t n) { return n & 0xff; }
+influxdb_if_inline constexpr uint16_t bit_swap16(uint16_t n) {
+  return bit_swap8(n) << 8 | bit_swap8(n >> 8);
+}
+influxdb_if_inline constexpr uint32_t bit_swap32(uint32_t n) {
+  return bit_swap16(n) << 16 | bit_swap16(n >> 16);
+}
+
+static constexpr int http_iv_reserve = 4;
+influxdb_if_inline int http_request_v_(
+    int sock, string_view_ref pref_header, struct iovec *iv, int iv_length, int body_size, std::string *resp) {
+  static const uint32_t rn = uint32_t('\r') << 8 | uint32_t('\n');
+  static const uint32_t rn_rn = rn << 16 | rn;
+  static const uint32_t rn_co = rn << 16 | uint32_t('C') << 8 | uint32_t('o');
+  static const uint32_t rn_tr = rn << 16 | uint32_t('T') << 8 | uint32_t('r');
+  static const int max_length = 128;
+  ssize_t recv_res = 0;
+  int ret_code = 0, pref = pref_header.size(), content_length = body_size;
+  int target, rn_co_pos = 0, rn_tr_pos = 0;
+  char buf[max_length];
+
+  // send data
+  std::string content_length_s = std::to_string(content_length);
+  iv[0].iov_base = (void *)(&pref_header[0]);
+  iv[0].iov_len = size_t(pref);
+  iv[1].iov_base = (void *)("Content-Length: ");
+  iv[1].iov_len = size_t(sizeof("Content-Length: ") - 1);
+  iv[2].iov_base = (void *)(&content_length_s[0]);
+  iv[2].iov_len = content_length_s.size();
+  iv[3].iov_base = (void *)("\r\n\r\n");
+  iv[3].iov_len = size_t(4);
+  int r = influx_http_writev(sock, iv, iv_length);
+  if (r < ssize_t(
+              iv[0].iov_len + iv[1].iov_len + iv[2].iov_len + iv[3].iov_len + body_size)) {
+    return -6;
+  }
+
+  uint32_t window4 = 0;
+  int i = 0, j = 0, recv_rest = max_length;
+  auto getOnce = [&] {
+    return (recv_res = influx_http_recv(
+                sock, &buf[0], std::min(max_length, recv_rest), 0)) > 0;
+  };
+
+  /**
+   * http message parser status:
+   *   0: skip http version
+   *   1: get status code
+   *   2: walk through header
+   *   3: parse header
+   *   4: get body
+   */
+  int status = 0;
+  if (resp) { // need http response
+    resp->clear();
+    target = 5;
+  } else {
+    target = 2;
+  }
+  while (status != target) {
+    if (recv_res == 0 && !getOnce()) {
+      break;
+    }
+    switch (status) {
+    default:
+      status = target;
+      break;
+    case 0:
+      for (i = 0; i < recv_res;) {
+        if (buf[i++] == ' ') {
+          goto status1;
+        }
+      }
+      recv_res = 0;
+      break;
+    case 1:
+      for (i = 0; i < recv_res; i++) {
+      status1:
+        if (std::isdigit(buf[i])) {
+          ret_code = ret_code * 10 + buf[i] - '0';
+        } else {
+          status = 2;
+          if (resp) {
+            resp->append(buf + i, recv_res - i);
+            i = 0;
+            goto status2;
+          }
+          recv_res -= i;
+          break;
+        }
+      }
+      recv_res = 0;
+      break;
+    case 2:
+      resp->append(buf, recv_res);
+    status2:
+      while (i < resp->size()) {
+        window4 = (window4 << 8) + (*resp)[i++];
+        switch (window4) {
+        case rn_rn:
+          status = 3;
+          pref = i;
+          goto status3;
+        case rn_co:
+          *(int32_t *)(&(*resp)[i - 4]) = rn_co_pos;
+          rn_co_pos = i;
+          break;
+        case rn_tr:
+          *(int32_t *)(&(*resp)[i - 4]) = rn_tr_pos;
+          rn_tr_pos = i;
+          break;
+        default:
+          break;
+        }
+      }
+      recv_res = 0;
+      break;
+    case 3:
+    status3 : {
+      const char *resp_addr = resp->c_str();
+      auto clearChain = [&resp_addr](int i, uint32_t val) {
+        while (i) {
+          auto pos = (int32_t *)&resp_addr[i - 4];
+          i = *pos;
+          *pos = val;
+        }
+      };
+      if (ret_code == 204) {
+        status = 5;
+      } else {
+        content_length = 0;
+        i = rn_co_pos;
+        while (i) {
+          if (macroConstStrCmpN(resp_addr + i, "ntent-Length: ") == 0) {
+            j = i + int(sizeof("ntent-Length: ")) - 1;
+            while (isdigit(resp_addr[j])) {
+              content_length = content_length * 10 + resp_addr[j++] - '0';
+            }
+          }
+          auto pos = (int32_t *)&resp_addr[i - 4];
+          i = *pos;
+        }
+        i = rn_tr_pos;
+        while (i) {
+          printf("%d\n", i);
+          auto pos = (int32_t *)&resp_addr[i - 4];
+          i = *pos;
+          abort();
+        }
+        if (content_length) {
+          recv_rest = content_length - int(resp->size()) + pref;
+          status = recv_rest ? 4 : 5;
+        } else {
+          status = 5;
+        }
+      }
+      clearChain(rn_co_pos, bit_swap32(rn_co));
+      clearChain(rn_tr_pos, bit_swap32(rn_tr));
+      (*resp)[pref - 2] = 0;
+      (*resp)[pref - 1] = 0;
+      printf("%s\n", resp_addr);
+      recv_res = 0;
+      break;
+    }
+    case 4:
+      if (recv_rest < recv_res) {
+        abort();
+      }
+      resp->append(buf, recv_res);
+      recv_rest -= recv_res;
+      if (recv_rest == 0) {
+        status = 0;
+      }
+      break;
+    }
+  }
+  return recv_res < 0 ? int(recv_res) : ret_code;
+}
+
+influxdb_if_inline int http_request_(
+    int sock, string_view_ref pref_header, string_view_ref body, std::string *resp) {
+  struct iovec iv[1+http_iv_reserve]{};
+  iv[4].iov_len = body.size();
+  iv[4].iov_base = (void*)&body[0];
+  return http_request_v_(sock, pref_header, iv, 5, body.size(), resp);
+}
+
+influxdb_if_inline int create_socket(const struct sockaddr_in *addr) {
+  int sock;
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    return -2;
+
+  if (connect(sock, (struct sockaddr *)(addr), sizeof(*addr)) < 0) {
+    closesocket(sock);
+    return -3;
+  }
+  return sock;
+}
+
+influxdb_if_inline int http_request(
+    const struct sockaddr_in *addr, string_view_ref pref_header,
+    string_view_ref body,
+    std::string *resp) {
+  int sock = create_socket(addr);
+  if (sock < 0) {
+    return sock;
+  }
+  int ret = http_request_(sock, pref_header, body, resp);
+  closesocket(sock);
+  return ret;
+}
+
+influxdb_if_inline unsigned char to_hex(unsigned char x) {
+  return x > 9 ? x + 55 : x + 48;
+}
+
+influxdb_if_inline void url_encode(std::string &out, const std::string &src) {
+  size_t pos, start = 0;
+  while (
+      (pos = src.find_first_not_of(
+           "abcdefghijklmnopqrstuvwxyqABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.~",
+           start)) != std::string::npos) {
+    out.append(src.c_str() + start, pos - start);
+    if (src[pos] == ' ')
+      out += "+";
+    else {
+      out += '%';
+      out += to_hex((unsigned char)src[pos] >> 4);
+      out += to_hex((unsigned char)src[pos] & 0xF);
+    }
+    start = ++pos;
+  }
+  out.append(src.c_str() + start, src.length() - start);
+}
+
+} // namespace detail
+} // namespace influx_client
+
+#endif // INFLUXDBM_HTTP_H
